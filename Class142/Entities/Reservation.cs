b76ce6cae2f3a4d3ea76010caf73d601f9b63602
Lucas Palmieri <lucas.palmieri@outlook.com>
2019-06-05 16:43:13 -0300

    Tratamento de exception Udemy base.

---
+++ Class142/Entities/Reservation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Class142.Entities.Exceptions;
+
+namespace Class142.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime ChekIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut < checkIn)
+            {
+                throw new DomainException("Error na reserva ! 2");
+            }
+
+            RoomNumber = roomNumber;
+            ChekIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(ChekIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException("Error na reserva ! 1");
+            }
+            else if (checkOut < checkIn)
+            {
+                throw new DomainException("Error na reserva ! 2");
+            }
+
+            ChekIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + ChekIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+}
---
+++ Class142/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Class142.Entities;
+using Class142.Entities.Exceptions;
+
+namespace Class142
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter date to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Erro: "+ e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format Erro: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Format Erro: " + e.Message);
+            }
+        }
+    }
+}
